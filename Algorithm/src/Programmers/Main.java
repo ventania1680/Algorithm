c8a160160b94a0d8dab84cf48d006bb9399b959c
ventania1680 <ventania1680@gmail.com>
2020-07-13 18:16:01 +0900

    algorithm

---
+++ Algorithm/src/Programmers/Main.java
@@ -0,0 +1,9 @@
+// For debug
+package Programmers;
+
+public class Main {
+	public static void main(String[] args) {
+		String arrangement = "()(((()())(())()))(())";
+		System.out.print(SQ04.solution(arrangement));
+	}
+}
