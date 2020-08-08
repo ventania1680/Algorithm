7bda70da4020a9abc285ca2e3bc8f234333c6ab5
ventania1680 <ventania1680@gmail.com>
2020-08-08 12:29:35 +0900

    BOJ1436/영화감독 숌/브루트포스 (수정1)

--- Algorithm/src/BOJ/BOJ1436.java
+++ Algorithm/src/BOJ/BOJ1436.java
@@ -6,8 +6,6 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class BOJ1436 {
     public static void solution() throws Exception {
@@ -15,19 +13,21 @@ public class BOJ1436 {
         int n = Integer.parseInt(br.readLine());
 
         final int doom = 666;
-        List<Integer> list = new ArrayList<>();
-        list.add(doom);
-        int tmp;
+        if (n == 1) {
+            System.out.println(doom);
+            return;
+        }
+
+        int tmp, count = 1;
         for (int i = 1666; i < 3000000; i++) {
             tmp = i;
             while (tmp >= 666) {
                 if ((tmp - doom) % 1000 == 0) {
-                    list.add(i);
+                    if (++count == n) System.out.println(i);
                     break;
                 }
                 tmp /= 10;
             }
         }
-        System.out.println(list.get(n - 1));
     }
 }
