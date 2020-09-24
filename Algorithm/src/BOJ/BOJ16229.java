b4e1d7d87dc8e8bc9007497720298de54164cbaa
ventania1680 <ventania1680@gmail.com>
2020-09-24 16:34:47 +0900

    BOJ16229/반복 패턴/KMP (수정2)

--- Algorithm/src/BOJ/BOJ16229.java
+++ Algorithm/src/BOJ/BOJ16229.java
@@ -31,16 +31,16 @@ public class BOJ16229 {
         if (n <= k) {
             answer = n;
         } else {
+            int pLen = 0, pCnt;
             int[] pi = getPi(str);
-            System.out.println(Arrays.toString(pi));
-            if (pi[n-1] != 0) {
-                if (n - pi[n-1] <= pi[n-1]+k) {
-                    answer = n - pi[n - 1];
-                    int len = answer;
-                    while (n+k >= (answer+len)*2) {
-                        answer += len;
-                    }
-                }
+            int cur = n-1;
+            while(true) {
+                pLen = n - pi[cur];
+                pCnt = (n + k) / pLen;
+                if(pCnt >= 2 && pLen*pCnt >= n)
+                    answer = Math.max(answer, pLen);
+                if (2*pi[cur] <= cur+1) break;
+                cur = pi[cur]-1;
             }
         }
         System.out.println(answer);
