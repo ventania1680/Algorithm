6dc3dbfc0bc8b4a114bafabe7261b6209878f21b
ventania1680 <ventania1680@gmail.com>
2020-10-08 14:26:54 +0900

    P12987/숫자게임/정렬

--- Algorithm/src/Programmers/Main.java
+++ Algorithm/src/Programmers/Main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		int[][] routes = {{-30000, 0}, {0, 30000}, {-1, 1}, {5, 10}};
-		System.out.print(P42884.solution(routes));
+		int[] A = {2,2,2,2};
+		int[] B = {1,1,1,1};
+		System.out.print(P12987.solution(A, B));
 	}
 }
